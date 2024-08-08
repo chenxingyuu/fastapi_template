0d4fb29491ac7bf779761a171df9138b41deb193
chenxingyu <chenxingyu@ndnu.edu.cn>
2024-08-08 10:58:14 +0800

    修复排序bug

--- cores/paginate.py
+++ cores/paginate.py
@@ -1,4 +1,5 @@
-from typing import Generic, List, Optional, Type, TypeVar, Union
+from dataclasses import dataclass
+from typing import Generic, List, Type, TypeVar
 
 from fastapi import Query
 from ghkit.enum import GEnum
@@ -21,18 +22,19 @@ class PageModel(BaseModel, Generic[T]):
     limit: int
 
 
-class PaginationParams(BaseModel):
+@dataclass
+class PaginationParams:
     page: int = Query(1, alias="page", ge=1)
     limit: int = Query(10, alias="limit", ge=1)
-    sort_by: Optional[List[str]] = Query(None, alias="sort_by")
-    sort_order: Union[List[OrderType], OrderType] = Query(OrderType.DESC, alias="sort_order")
+    sort_by: List[str] = Query(None, alias="sort_by")
+    sort_order: List[OrderType] = Query(None, alias="sort_order")
 
     def apply_sorting(self, queryset: QuerySet[T]) -> QuerySet[T]:
         if not self.sort_by:
             return queryset
 
-        if isinstance(self.sort_order, OrderType):
-            self.sort_order = [self.sort_order]
+        if self.sort_order is None:
+            self.sort_order = [OrderType.DESC]
 
         if len(self.sort_order) < len(self.sort_by):
             self.sort_order = [*self.sort_order, *self.sort_order[-1:] * (len(self.sort_by) - len(self.sort_order))]
